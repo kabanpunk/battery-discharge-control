996707da1d46d08761ae74372e8f3ed7424649aa
kabanpunk <lik42127@gmail.com>
2020-02-04 18:16:41 +0700

    Update main.ino
    improved function, added output without char

--- main/main.ino
+++ main/main.ino
@@ -1,57 +1,47 @@
-#include <Arduino.h>
-#include <TM1637Display.h>
-
-// Module connection pins (Digital Pins)
-#define CLK 12
-#define DIO 11
-
-// The amount of time (in milliseconds) between tests
-#define TEST_DELAY   2000
-
-const uint8_t A = SEG_E | SEG_F | SEG_A | SEG_B | SEG_C | SEG_G;
-const uint8_t U = SEG_E | SEG_F | SEG_B | SEG_C | SEG_D;
-const uint8_t t = SEG_E | SEG_F | SEG_G | SEG_D;
-
-TM1637Display display(CLK, DIO);
-
-float sr(float f, int n) {
-  return round(f * pow(10, n)) / pow(10, n);
-}
-void printFloat(float f, uint8_t C) {
+void printFloat(float f, uint8_t C = 0x00) {
   uint8_t data[] = { 0x00 , 0x00, 0x00, C };
   String s = String(f);
-
+  int tip = String(int(f)).length();
   int di = s.indexOf('.');
-  String ns = String(sr(f, 4 - di));
-  ns.remove(ns.indexOf('.'), 1);
-  int N = ns.length();
-  if (N > 0) {
-    data[2] = display.encodeDigit(int(ns[2]));
-  }
-  if (N > 1) {
-    data[1] = display.encodeDigit(int(ns[1]));
-  }
-  if (N > 2) {
-    data[0] = display.encodeDigit(int(ns[0]));
-  }
-  if (String(int(f)).length()<3)
-    data[di - 1] += 0x80;
-  display.setSegments(data);
-}
 
-void setup()
-{
-  Serial.begin(9600);
-  display.setBrightness(0x0f);
-  for (int i = 9900; i < 100000; i++) {
-    float f = i / 100.;
-    printFloat(f);
-    Serial.println(f);
-    delay(10);
+  if (C == 0x00) { 
+    String ns = delzeros(String(sr(f, 4 - tip)));
+    Serial.println(ns);
+    ns.remove(ns.indexOf('.'), 1);
+    int N = ns.length();
+    int c = 0;
+    if (N > 0) {
+      data[3] = display.encodeDigit(int(ns[N - 1])); c++;
+    }
+    if (N > 1) {
+      data[2] = display.encodeDigit(int(ns[N - 2])); c++;
+    }
+    if (N > 2) {
+      data[1] = display.encodeDigit(int(ns[N - 3])); c++;
+    }
+    if (N > 3) {
+      data[0] = display.encodeDigit(int(ns[N - 4])); c++;
+    }
+    Serial.println("di: " + String(di) + "; c: " + String(c));
+    if (di > 0)
+      data[ di + 3 - c] += 0x80;
+    display.setSegments(data);
   }
-
-}
-
-void loop()
-{
+  else {
+    String ns = String(sr(f, 4 - di));
+    ns.remove(ns.indexOf('.'), 1);
+    int N = ns.length();
+    if (N > 0) {
+      data[2] = display.encodeDigit(int(ns[2]));
+    }
+    if (N > 1) {
+      data[1] = display.encodeDigit(int(ns[1]));
+    }
+    if (N > 2) {
+      data[0] = display.encodeDigit(int(ns[0]));
+    }
+    if (di > 0)
+      data[ di - 1 ] += 0x80;
+    display.setSegments(data);
+  }
 }
