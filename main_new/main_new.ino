1be6bd663d87e79b9fdab9b4b6a7aa9f862b6103
kabanpunk <lik42127@gmail.com>
2020-05-26 23:52:22 +0700

    remastered version
    resistance print works correctly in [0, 99]
    if reststance > 99, like a 100 or 123 etc, print will be like 10 and 12.
    To fix it, u should remove 27-30 lines. but with it u will loose pretty print amperage with zeros in end (if amperage is integer)

--- main_new/main_new.ino
+++ main_new/main_new.ino
@@ -3,9 +3,6 @@
 #include <Wire.h>
 #include <Adafruit_ADS1015.h>
 
-float VOLTAGE_THRESHOLD = 2.6;
-float CAPACIRY_THRESHOLD = 0.05;
-
 Adafruit_ADS1115 ads;
 
 // Module connection pins (Digital Pins)
@@ -17,29 +14,54 @@ Adafruit_ADS1115 ads;
 
 const uint8_t A = SEG_E | SEG_F | SEG_A | SEG_B | SEG_C | SEG_G;
 const uint8_t V = SEG_E | SEG_F | SEG_B | SEG_C | SEG_D;
-const uint8_t R = SEG_E | SEG_G;
 const uint8_t t = SEG_E | SEG_F | SEG_G | SEG_D;
+const uint8_t R = SEG_E | SEG_G;
 
 TM1637Display display(CLK, DIO);
 
+float sr(float f, int n) {
+  return round(f * pow(10, n)) / pow(10, n) / 1.;
+}
 
-float amperage = 0;
-float voltage = 0;
-float resistance = 0;
-float capaciry = 0;
-bool toggle = false;
-long work_time = 0;
-bool end_of_cycle_check = false;
-long cycle_start_time = 0;
-float cycle_end_time = 0;
-bool dump_existence = false;
+String remove_zeros(String s, int l) {
+  if (s[s.length() - 1] == '0' and s[s.length() - 2] == '0') { //((s.indexOf('.') !=  s.length() - 2 ? s.length() : s.length()  - 1) <= l) {
+    s.remove(s.length() - 1);
+    return s;
+  }
+  int i = s.length() - 1;
+  while (i > 0 and (s[i] == '0' or s[i] == '.')) {
+    if (s[i] == '.') {
+      s.remove(i);
+      break;
+    }
+    s.remove(i);
+    i -= 1;
+  }
+  return s;
+}
 
-void update_ads() {
-  amperage = abs(ads.readADC_Differential_0_1() * 0.015625F / 2.);
-  voltage = abs(ads.readADC_Differential_2_3() * 0.1875F / 1000.);
-  //amperage = (amperage < 0.5) ? 0. : amperage;
-  voltage = (voltage < 0.5) ? 0. : voltage;
-  capaciry = (amperage * millis()) / 3600000.;
+void printFloat(float x, int l, uint8_t C = 0x00) {
+  uint8_t data[] = { 0x00 , 0x00, 0x00, C };
+  String s = String( sr( x, l - String(int(x)).length() ) );
+
+  s = remove_zeros(s, l);
+  int i = l - 1, j = s.length() - 1;
+  while (i >= 0 and j >= 0) {
+    if (s[j] == '.') {
+      data[i] += 0x80;
+      j -= 1;
+    }
+    data[i] += display.encodeDigit(int(s[j]));
+    i -= 1;
+    j -= 1;
+  }
+  display.setSegments(data);
+}
+
+String float_to_string(float x) {
+  String s = String(round(x * 1000000));
+  s = s.substring(0, String(x).indexOf('.')) + '.' + s.substring(String(x).indexOf('.'), s.length());
+  return s;
 }
 
 void setup()
@@ -64,63 +86,88 @@ void setup()
   sei();
 
   ads.begin();
-  Serial.println("Ожидание начала цикла");
+
+  //Serial.println("Ожидание начала цикла");
+}
+
+float amperage = 0;
+float voltage = 0;
+float resistance = 0;
+bool toggle = false;
+double work_time = 0;
+bool end_of_cycle_check = false;
+long cycle_start_time = 0;
+double cycle_end_time = 0;
+double BATTERY_CAPACITY = 0;
+double CAPACITY_THRESHOLD  = 0.05;
+double VOLTAGE_THRESHOLD = 2.6; 
+bool is_a_dump_recorded = false;
+ 
+void dump_record() {
+  float sum_a = 0, sum_v = 0;
+  int K = 30;
+  //Serial.println("\n\n");
+  for (int i = 0; i < K; i++) {
+    //Serial.println("Напряжение: " + String(voltage, 6) + "; Сила тока: " + String(amperage, 6)  );
+    sum_a += amperage;
+    sum_v += voltage;
+    read_ADC();
+  }
+  //Serial.println("Среднее напряжение: " + String(sum_v / K, 6) + "; Средняя сила тока: " + String(sum_a / K, 6));
+  //Serial.println("----------------------------------");
+
+  digitalWrite(5, LOW);
+  sum_a = 0, sum_v = 0;
+  for (int i = 0; i < K; i++) {
+    //Serial.println("Напряжение: " + String(voltage, 6) + "; Сила тока: " + String(amperage, 6)  );
+    sum_a += amperage;
+    sum_v += voltage;
+    read_ADC();
+  }
+  //Serial.println("Среднее напряжение: " + String(sum_v / K, 6) + "; Средняя сила тока: " + String(sum_a / K, 6));
+  //Serial.println("----------------------------------");
+  float middle_v1 = sum_v / K;
+
+  digitalWrite(5, HIGH);
+  sum_a = 0, sum_v = 0;
+  for (int i = 0; i < K; i++) {
+    //Serial.println("Напряжение: " + String(voltage, 6) + "; Сила тока: " + String(amperage, 6)  );
+    sum_a += amperage;
+    sum_v += voltage;
+    read_ADC();
+  }
+  //Serial.println("Среднее напряжение: " + String(sum_v / K, 6) + "; Средняя сила тока: " + String(sum_a / K, 6));
+  //Serial.println("----------------------------------");
+  resistance = (middle_v1 - (sum_v / K)) * 1000; //миллиом
+  Serial.println("Сопротивление: " + String(resistance, 6));
+  Serial.println("\n\n");
+
+  is_a_dump_recorded = true;
+}
+
+void read_ADC() {
+  voltage = abs(ads.readADC_Differential_2_3() * 0.1875F / 1000.);
+  amperage = abs(ads.readADC_Differential_0_1() * 0.015625F / 2.);
+  amperage = amperage < 0.5 ? 0 : amperage;
+  double BATTERY_CAPACITY_MS = amperage * (millis() - cycle_start_time);
+  BATTERY_CAPACITY = BATTERY_CAPACITY_MS < 36000 ? 0 : BATTERY_CAPACITY_MS / 3600000.;
 }
 
 void loop()
 {
-  update_ads();
-
-  if (!dump_existence and capaciry > CAPACIRY_THRESHOLD) {
-    float sum_a = 0, sum_v = 0;
-    int K = 10;
-    Serial.println("\n\n");
-    for (int i = 0; i < K; i++) {
-      Serial.println("Напряжение: " + String(voltage, 6) + "; Сила тока: " + String(amperage, 6)  );
-      sum_a += amperage;
-      sum_v += voltage;
-      update_ads();
-    }
-    Serial.println("Среднее напряжение: " + String(sum_v / K, 6) + "; Средняя сила тока: " + String(sum_a / K, 6));
-    Serial.println("----------------------------------");
-
-    digitalWrite(5, LOW);
-    sum_a = 0, sum_v = 0;
-    for (int i = 0; i < K; i++) {
-      Serial.println("Напряжение: " + String(voltage, 6) + "; Сила тока: " + String(amperage, 6)  );
-      sum_a += amperage;
-      sum_v += voltage;
-      update_ads();
-    }
-    Serial.println("Среднее напряжение: " + String(sum_v / K, 6) + "; Средняя сила тока: " + String(sum_a / K, 6));
-    Serial.println("----------------------------------");
-    float middle_v1 = sum_v / K;
-
-    digitalWrite(5, HIGH);
-    sum_a = 0, sum_v = 0;
-    for (int i = 0; i < K; i++) {
-      Serial.println("Напряжение: " + String(voltage, 6) + "; Сила тока: " + String(amperage, 6)  );
-      sum_a += amperage;
-      sum_v += voltage;
-      update_ads();
-    }
-    Serial.println("Среднее напряжение: " + String(sum_v / K, 6) + "; Средняя сила тока: " + String(sum_a / K, 6));
-    Serial.println("----------------------------------");
-    resistance = (middle_v1 - (sum_v / K)) * 1000; //милливольт
-    Serial.println("Сопротивление: " + String(resistance, 6));
-    Serial.println("\n\n");
-
-    dump_existence = true;
-  }
-
-
+  read_ADC();
   if (toggle and !end_of_cycle_check and voltage < VOLTAGE_THRESHOLD) {
     cycle_end_time = (millis() - cycle_start_time) / 60000.;
     toggle = false;
     end_of_cycle_check = true;
   }
   if (toggle) {
-    digitalWrite(5, HIGH);
+    if (!is_a_dump_recorded and CAPACITY_THRESHOLD > BATTERY_CAPACITY) { 
+      dump_record();
+    }
+    else {
+      digitalWrite(5, HIGH);
+    }
   }
   else {
     digitalWrite(5, LOW);
@@ -129,48 +176,48 @@ void loop()
 
 int screen_counter = 0;
 void flipping_screens(int mode = 0) {
-  if (mode < 0) {
-    if (screen_counter == 0) {
-      screen_counter ++;
-      display.printFloat(cycle_end_time, 4);
-    }
-    else if (screen_counter == 1) { 
-      display.printFloat(resistance, 3, R); 
-      screen_counter = 0;
-    }
+  if (screen_counter == 0) {
+    screen_counter ++;
+    printFloat(voltage, 3, V);
   }
-  else {
-    if (screen_counter == 0) {
-      screen_counter ++;
-      display.printFloat(voltage, 3, V);
-    }
-    else if (screen_counter == 1) {
-      screen_counter ++;
-      display.printFloat(amperage, 3, A);
-      if (mode == 0)
-        screen_counter = 0;
-    }
-    else {
+  else if (screen_counter == 1) {
+    screen_counter ++;
+    printFloat(amperage, 3, A);
+    if (mode == 0)
       screen_counter = 0;
-      display.printFloat((millis() - cycle_start_time) / 60000., 4);
-    }
+  }
+  else if (mode == 1) {
+    screen_counter = 0;
+    printFloat((millis() - cycle_start_time) / 60000., 4);
+  }
+}
+
+int sc_end = 0;
+void flipping_screens_end() {
+  if (sc_end == 0) {
+    sc_end ++;
+    display.printFloat(cycle_end_time, 4);
+  }
+  else if (sc_end == 1) {
+    display.printFloat(resistance, 3, R);
+    sc_end = 0;
   }
 }
 
 ISR(TIMER1_COMPA_vect) {
   if (end_of_cycle_check) {
     Serial.println("Конец цикла");
-    //Serial.println("Время: " + String(cycle_end_time));
-    flipping_screens(-1);
+    Serial.println("Время: " + String(cycle_end_time));
+    flipping_screens_end();
   }
   else if (toggle) {
     Serial.println("Режим разрядки");
-    Serial.println("Напряжение: " + String(voltage, 6)  + "; Сила тока: " + String(amperage, 6) + "; Ёмкость: " + String(capaciry, 6)  + "; Время: " + String((millis() - cycle_start_time) / 60000.));
+    Serial.println("Напряжение: " + String(voltage) + "; Сила тока: " + amperage + "; Время: " + String((millis() - cycle_start_time) / 60000.));
     flipping_screens(1);
   }
   else {
     Serial.println("Режим ожидания");
-    Serial.println("Напряжение: " + String(voltage, 6)  + "; Сила тока: " + String(amperage, 6));
+    Serial.println("Напряжение: " + String(voltage) + "; Сила тока: ");
     flipping_screens(0);
   }
   Serial.println("------------------\n");
@@ -191,10 +238,11 @@ void btn() {
         toggle = false;
         Serial.println("Режим сменён на " + String(toggle) + " (0 - батарея не разрежается; 1 - разрежается)");
       }
-      else {
+      else if (voltage > 1) {
         end_of_cycle_check = false;
         cycle_start_time = millis();
         toggle = true;
+        is_a_dump_recorded = false;
         Serial.println("Режим сменён на " + String(toggle) + " (0 - батарея не разрежается; 1 - разрежается)");
       }
     }
